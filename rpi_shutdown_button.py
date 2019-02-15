0bebf24e1668249dc1bf3e74182a38821dc4990a
mongoq <mongoq@das-labor.org>
2019-02-15 18:56:43 +0100

    Update rpi_shutdown_button.py

--- rpi_shutdown_button.py
+++ rpi_shutdown_button.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
-import time
 
 GPIO.setmode(GPIO.BCM)
 
@@ -11,6 +10,5 @@ while True:
     input_state = GPIO.input(18)
     if input_state == False:
         print('Shutdown initiated ...')
-        time.sleep(1)
         from subprocess import call
         call("shutdown -h now", shell=True)
